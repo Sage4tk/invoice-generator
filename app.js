5fd4c2e99834a86522d7d2d6143d58ecb62e1bcc
timothy timbol <ttimbol08@gmail.com>
2022-02-23 18:23:59 +0400

    finished product

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib;
 
+const inputs = document.querySelectorAll('input')
 
 //get and add text to pdf
 const editPDF = async () => {
@@ -18,8 +19,25 @@ const editPDF = async () => {
 
     //get width and height
     const { width, height } = firstPage.getSize();
+    
+    //add upper text
+    firstPage.drawText(inputs[0].value, {
+        x:135,
+        y: height - 227.5,
+        size: 12,
+        font,
+        color: rgb(0,0,0)
+    });
 
-    //add texts
+    firstPage.drawText(inputs[1].value, {
+        x:401,
+        y: height - 227.5,
+        size: 12,
+        font,
+        color: rgb(0,0,0)
+    });
+
+    //add middle texts
     firstPage.drawText("Mr. Joel Timbol", {
         x:90,
         y: height - 300,
@@ -71,9 +89,30 @@ const editPDF = async () => {
         color: rgb(0,0,0)
     });
 
+    firstPage.drawText(inputs[2].value, {
+        x:261,
+        y: height - 521,
+        size: 12,
+        font,
+        color: rgb(0,0,0)
+    });
+
+    firstPage.drawText(inputs[3].value, {
+        x:343,
+        y: height - 521,
+        size: 12,
+        font,
+        color: rgb(0,0,0)
+    });
+
     const pdfBytes = await pdfDoc.save();
 
     //download
     download(pdfBytes, "invoice.pdf", "application/pdf");
 
+}
+
+document.querySelector("form").onsubmit = (e) => {
+    e.preventDefault();
+    editPDF();
 }
